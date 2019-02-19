0874fee3ce89e670c5032c60bba635beff26f446
Preston Davis <35507470+premdav@users.noreply.github.com>
2019-02-19 09:03:53 -0600

    added bcrypt-nodejs as a dependency

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
 
 const app = express();
 
@@ -10,7 +11,6 @@ const database = {
             id: '123',
             name: 'Joe',
             email: 'john@gmail.com',
-            password: 'cookies',
             entries: 0,
             joined: new Date()
         },
@@ -18,9 +18,15 @@ const database = {
             id: '124',
             name: 'Sally',
             email: 'sally@gmail.com',
-            password: 'bananas',
             entries: 0,
             joined: new Date()
+        }
+    ],
+    login: [
+        {
+            id: '987',
+            hash: '',
+            email: 'john@gmail.com'
         }
     ]
 }
@@ -30,6 +36,13 @@ app.get('/', (req, res) => {
 });
 
 app.post('/signin', (req, res) => {
+    bcrypt.compare("apples", '$2a$10$gvNE0W26Ptiw1FBY3O3wlOcSQ/V8IuPO0Qk6R9Ydq5G97fsHRdAKu', function(err, res) {
+        console.log('first guess', res);
+    });
+    bcrypt.compare("veggies", '$2a$10$gvNE0W26Ptiw1FBY3O3wlOcSQ/V8IuPO0Qk6R9Ydq5G97fsHRdAKu', function(err, res) {
+        console.log('second guess', res);
+    });
+
     if(req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
         res.json('success');
     } else {
@@ -39,6 +52,7 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
     const { email, name, password } = req.body;
+
     database.users.push({
         id: '125',
         name: name,
@@ -78,6 +92,13 @@ app.put('/image', (req, res) => {
         res.status(400).json('not found');
     }
 });
+
+// bcrypt.hash("bacon", null, null, function(err, hash) {
+//     // Store hash in your password DB.
+// });
+
+// // Load hash from your password DB.
+
 
 app.listen(3000, () => {
     console.log('app is running on port 3000');
