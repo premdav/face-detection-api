0e1b3785ebc7112bd7e172de2e89ec2d0d366663
Preston Davis <35507470+premdav@users.noreply.github.com>
2019-02-21 10:37:24 -0600

    Image put request now connecting to db and incrementing entry count

--- server.js
+++ server.js
@@ -91,17 +91,14 @@ app.get('/profile/:id', (req, res) => {
 
 app.put('/image', (req, res) => {
     const { id } = req.body;
-    let found = false;
-    database.users.forEach(user => {
-        if(user.id === id) {
-            found = true;
-            user.entries++;
-           return res.json(user.entries);
-        }
-    });
-    if(!found) {
-        res.status(400).json('not found');
-    }
+
+   db('users').where('id', '=', id)
+   .increment('entries', 1)
+   .returning('entries')
+   .then(entries => {
+       res.json(entries[0]);
+   })
+   .catch(err => res.status(400).json('Unable to get entry count'));
 });
 
 // bcrypt.hash("bacon", null, null, function(err, hash) {
