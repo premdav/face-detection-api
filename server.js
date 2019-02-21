99f0cc59b10e97ac6767fb04b393eb79533aea91
Preston Davis <35507470+premdav@users.noreply.github.com>
2019-02-21 12:41:30 -0600

    registration using bcrypt and sign in connected to db

--- server.js
+++ server.js
@@ -10,70 +10,61 @@ const db = knex({
     connection: dbLogin
   });
 
-//   db.select('*').from('users')
-//   .then(data => {
-//       console.log(data);
-//   });
-
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
-
-const database = {
-    users: [
-        {
-            id: '123',
-            name: 'John',
-            password: 'cookies',
-            email: 'john@gmail.com',
-            entries: 0,
-            joined: new Date()
-        },
-        {
-            id: '124',
-            name: 'Sally',
-            password: 'bananas',
-            email: 'sally@gmail.com',
-            entries: 0,
-            joined: new Date()
-        }
-    ],
-    login: [
-        {
-            id: '987',
-            hash: '',
-            email: 'john@gmail.com'
-        }
-    ]
-}
 
 app.get('/', (req, res) => {
     res.send(database.users);
 });
 
 app.post('/signin', (req, res) => {
-    if(req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
-        res.json(database.users[0]);
-    } else {
-        res.status(400).json('error logging in');
-    }
+    db.select('email', 'hash').from('login')
+    .where('email', '=', req.body.email)
+    .then(data => {
+        const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+        if(isValid) {
+            return db.select('*').from('users')
+            .where('email', '=', req.body.email)
+            .then(user => {
+                res.json(user[0]);
+            })
+            .catch(err => res.status(400).json('Unable to get user'));
+        } else {
+            res.status(400).json('Wrong username or password');
+        }
+    })
+    .catch(err => res.status(400).json('Wrong username or password'));
 });
 
 app.post('/register', (req, res) => {
     const { email, name, password } = req.body;
-
-    db('users')
-    .returning('*')
-    .insert({
-        email: email,
-        name: name,
-        joined: new Date()
-    })
-    .then(user => {
-        res.json(user[0]);
-    })
-    .catch(err => res.status(400).json('unable to register'));
+    // storing password
+    const hash = bcrypt.hashSync(password);
+        db.transaction(trx => {
+            trx.insert({
+                hash: hash,
+                email: email
+            })
+            .into('login')
+            .returning('email')
+            .then(loginEmail => {
+                return trx('users')
+                    .returning('*')
+                    .insert({
+                        email: loginEmail[0],
+                        name: name,
+                        joined: new Date()
+                    })
+                    .then(user => {
+                        res.json(user[0]);
+                    });
+                })
+                .then(trx.commit)
+                .catch(trx.rollback);
+            })
+        .catch(err => res.status(400).json('unable to register'));
 });
 
 app.get('/profile/:id', (req, res) => {
@@ -100,13 +91,6 @@ app.put('/image', (req, res) => {
    })
    .catch(err => res.status(400).json('Unable to get entry count'));
 });
-
-// bcrypt.hash("bacon", null, null, function(err, hash) {
-//     // Store hash in your password DB.
-// });
-
-// // Load hash from your password DB.
-
 
 app.listen(3000, () => {
     console.log('app is running on port 3000');
