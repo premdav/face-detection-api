4bce508b568a80c9899bc4c04c2e7d36323fe389
Preston Davis <35507470+premdav@users.noreply.github.com>
2019-02-21 10:15:54 -0600

    Profile path connected to database

--- server.js
+++ server.js
@@ -10,10 +10,10 @@ const db = knex({
     connection: dbLogin
   });
 
-  db.select('*').from('users')
-  .then(data => {
-      console.log(data);
-  });
+//   db.select('*').from('users')
+//   .then(data => {
+//       console.log(data);
+//   });
 
 const app = express();
 
@@ -78,16 +78,15 @@ app.post('/register', (req, res) => {
 
 app.get('/profile/:id', (req, res) => {
     const { id } = req.params;
-    let found = false;
-    database.users.forEach(user => {
-        if(user.id === id) {
-            found = true;
-           return res.json(user);
+    db.select('*').from('users').where({ id })
+    .then(user => {
+        if(user.length) {
+           res.json(user[0]); 
+        } else {
+            res.status(400).json('Not found');
         }
-    });
-    if(!found) {
-        res.status(400).json('not found');
-    }
+    })
+    .catch(err => res.status(400).json('Error getting user'));
 });
 
 app.put('/image', (req, res) => {
