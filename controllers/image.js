838edf30648b9fee37ca0b089a06a0097abea73c
Preston Davis <35507470+premdav@users.noreply.github.com>
2019-02-21 14:48:00 -0600

    refactoring code

---
+++ controllers/image.js
@@ -0,0 +1,15 @@
+const handleImage = (req, res, db) => {
+    const { id } = req.body;
+
+   db('users').where('id', '=', id)
+   .increment('entries', 1)
+   .returning('entries')
+   .then(entries => {
+       res.json(entries[0]);
+   })
+   .catch(err => res.status(400).json('Unable to get entry count'));
+};
+
+module.exports = {
+    handleImage: handleImage
+};
