4648c31692e1d266c0e83a49aabf6a18b14b2e6b
Preston Davis <35507470+premdav@users.noreply.github.com>
2019-02-22 08:57:47 -0600

    added validation and api call now made on backend

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,18 @@
+const Clarifai = require('clarifai');
+const myApiKey = require('../config');
+
+const app = new Clarifai.App(myApiKey);
+
+const handleApiCall = (req, res) => {
+   app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => {
+        res.json(data);
+    })
+    .catch(err => res.status(400).json('unable to work with API'));
+}
+
+
 const handleImage = (req, res, db) => {
     const { id } = req.body;
 
@@ -11,5 +26,6 @@ const handleImage = (req, res, db) => {
 };
 
 module.exports = {
-    handleImage: handleImage
+    handleImage,
+    handleApiCall
 };
--- server.js
+++ server.js
@@ -32,6 +32,8 @@ app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)});
 
 app.put('/image', (req, res) => {image.handleImage(req, res, db)});
 
+app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)});
+
 app.listen(3000, () => {
     console.log('app is running on port 3000');
 });
